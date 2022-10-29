99c9d9a198e94a74ad5dc1939dc07a8b5daf01cb
Gabriel Costa <gabrielestevan14@gmail.com>
2022-10-29 13:08:43 -0300

    create db connection

---
+++ src/database/connection.ts
@@ -0,0 +1,13 @@
+import { DataSource } from 'typeorm'
+
+export const postgresConnection = new DataSource({
+	type: 'postgres',
+	host: process.env.DB_HOST,
+	port: +process.env.DB_PORT,
+	username: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
+	entities: [`${__dirname}/**/entities/*.{ts, js}`],
+	migrations: [`${__dirname}/**/migrations/*.{ts, js}`],
+	synchronize: true
+})
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,16 @@
-const a = 'a'
+import 'reflect-metadata'
+import express from 'express'
+import helmet from 'helmet'
+import { postgresConnection } from './database/connection'
+import usersRoutes from './http/routes/users.routes'
+
+postgresConnection.initialize().then(() => {
+  const app = express()
+
+  app.use(helmet())
+  app.use(express.json())
+
+  app.use(usersRoutes)
+
+  app.listen(3000, () => console.log('🚀 Listening on port 3000'))
+})
