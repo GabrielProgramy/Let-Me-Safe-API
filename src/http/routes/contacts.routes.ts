a219a4256456e6c7c4bccd4b8fa1589fda0d5687
Marcos Felipe S.L <marcos.lima52@fatec.sp.gov.br>
2023-02-02 00:40:36 -0300

    Contacts Repository CRUD

---
+++ src/http/routes/contacts.routes.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import ContactsController from "../controller/UsersController";
+import { verifyJWT } from "../middleware/verifyJWT";
+
+const contactsRoutes = Router();
+const controller = new ContactsController();
+
+usersRoutes.post("/auth", controller.authenticateContacts);
+usersRoutes.post("/", controller.createContacts);
+contactsRoutes.put("/me", verifyJWT, controller.updateUser);
+usersRoutes.get("/:userId", verifyJWT, controller.findUser);
+
+export default usersRoutes;
