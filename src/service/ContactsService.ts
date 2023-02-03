a5d1a025b160a49ab247822fea2026e56814daac
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2023-02-03 09:38:52 -0300

    Contacts Service CRUD

--- src/service/ContactsService.ts
+++ src/service/ContactsService.ts
@@ -8,7 +8,38 @@ export default class ContactsService {
     this.contactsRepository = new ContactsRepository();
   }
 
-  //   async create(contacts: Contacts): Promise<Contacts> {
-  //     const alreadyExistContacts = await this.findByEmail(contacts.email);
-  //   }
+  async createContact(contact: Contacts): Promise<Contacts> {
+    const alreadyExistsContact =
+      await this.contactsRepository.findContactByName(contact.name);
+
+    if (alreadyExistsContact) throw new Error("Contact already exists!");
+
+    return this.contactsRepository.insertContact(contact);
+  }
+
+  async findContactById(id: string): Promise<Contacts> {
+    const contact = await this.contactsRepository.findContactById(id);
+
+    if (!contact) throw new Error("Contact not found!");
+
+    return contact;
+  }
+
+  async updateContact(contact: Contacts): Promise<Contacts> {
+    await this.findContactById(contact.id);
+
+    const updateContact = await this.contactsRepository.updateContact(contact);
+
+    return updateContact;
+  }
+
+  async listContacts(userId: string): Promise<Contacts[]> {
+    const listContacts = await this.contactsRepository.listContacts(userId);
+
+    return listContacts;
+  }
+
+  async deleteContact(id: string): Promise<void> {
+    await this.contactsRepository.deleteContact(id);
+  }
 }
